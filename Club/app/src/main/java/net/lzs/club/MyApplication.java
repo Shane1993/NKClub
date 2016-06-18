fde0f63d9f26222f4753eb252c390ddc7759a43c
Shane1993 <513199010@qq.com>
2016-06-18 11:25:45 +0800

    Initial Image Loader

--- Club/app/src/main/java/net/lzs/club/MyApplication.java
+++ Club/app/src/main/java/net/lzs/club/MyApplication.java
@@ -1,7 +1,11 @@
 package net.lzs.club;
 
 import android.app.Application;
-import android.util.Log;
+
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
 import cn.bmob.v3.Bmob;
 
@@ -15,5 +19,21 @@ public class MyApplication extends Application
     {
         super.onCreate();
         Bmob.initialize(this,"2b58ede2367fe0ad8dcf90b9dc6cf391");
+
+        initImageLoader();
+    }
+
+    private void initImageLoader()
+    {
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(getApplicationContext())
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
+                .diskCacheSize(50 * 1024 * 1024)
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .writeDebugLogs()
+                .build();
+
+        ImageLoader.getInstance().init(configuration);
     }
 }
